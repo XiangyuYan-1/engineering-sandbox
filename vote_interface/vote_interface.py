7d65455675934b70f358023a13842780e9cce0ef
Luka-Rondal <luka.rondal@epfedu.fr>
2026-02-05 09:23:31 +0100

    Adding vote button

--- vote_interface/vote_interface.py
+++ vote_interface/vote_interface.py
@@ -2,6 +2,13 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 from datetime import datetime
 
+interface = 0
+
+
+def vote_button_click():
+    global interface
+    interface = 1
+
 
 def main():
     root = tk.Tk()
@@ -10,12 +17,15 @@ def main():
     root.minsize(820, 480)
     root.configure(bg="#03163D")
 
-    #header
+    # header
     header = tk.Frame(root, bg="#111827", padx=18, pady=14)
     header.pack(fill="x")
 
-    root.mainloop()
+    if interface == 0:
+        vote_button = ttk.Button(root, text="vote,", command=vote_button_click)
+        vote_button.pack(ipadx=5, ipady=5, expand=True)
 
+    root.mainloop()
 
 
 if __name__ == "__main__":
